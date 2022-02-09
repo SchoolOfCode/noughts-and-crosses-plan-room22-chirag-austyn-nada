2f6cc294b1a62f743ba8a9f2ef0dc6ba7ab6e8b9
Moazzam Begg <Moazzam1@Moazzams-Air.broadband>
2022-02-09 16:55:00 +0000

    completed

--- tic-tac-toe/src/components/App/App.js
+++ tic-tac-toe/src/components/App/App.js
@@ -1,32 +1,29 @@
 import { useState } from "react";
 import styles from "./App.module.css";
-import {calculateWinner} from "../../helper"
+import { calculateWinner } from "../../helper";
 import Board from "../Board";
 
 function App() {
   const [board, setBoard] = useState([Array(9).fill(null)]);
-  const [isPlayerX, setIsPlayerX] = useState(true)
-  const winner = calculateWinner(board)
-  const currentPlayer
-  // }
-  // function playerMove(x) {
-  //   if (x === 0) {
-  //     return <p>X</p>;
-  //   } else if (x === 1) {
-  //     return <p>0</p>;
-  //   }
-  // }
+  const [isPlayerX, setIsPlayerX] = useState(true);
+  const winner = calculateWinner(board);
+  const currentPlayer = isPlayerX ? "X" : "O";
 
-//   function playerMove(event) {
-//     setMove("X");
-//   }
-
-//   console.log(move);
+  function handleSquareClick(index) {
+    if (winner || board[index]) {
+      return;
+    }
+    setBoard([
+      ...board.slice(0, index),
+      currentPlayer,
+      ...board.slice(index + 1),
+    ]);
+  }
 
   return (
     <div className={styles.App}>
       <h1>Tic-Tac-Toe</h1>
-      <Board />
+      <Board onSelectSquare={handleSquareClick} />
     </div>
   );
 }
