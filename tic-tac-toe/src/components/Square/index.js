ad6018fb402895f08afbf6298a7ac5e6043db9b6
Moazzam Begg <Moazzam1@Moazzams-Air.broadband>
2022-02-09 15:09:14 +0000

    Css

--- tic-tac-toe/src/components/Square/index.js
+++ tic-tac-toe/src/components/Square/index.js
@@ -6,15 +6,24 @@ import styles from "./Square.module.css";
 function Square(){
 const [move, setMove] = useState("") 
   
-  function playerMove(event) {
-    console.log("I'm clicked")
-    setMove("X");
+  // function playerMove(event) {
+  //   if (player === 1) { console.log("I'm clicked")
+  //   setMove("X");}
+  //   else if (player === 1) {
+  //     console.log("I'm clicked");
+  //     setMove("0");
+  //   }
+
+  // }
+
+  function playerMove(event){
+    setMove("X")
   }
 
 
   return (
     <div className={styles.Square} onClick={playerMove}>
-      {move}
+      <h1>{move}</h1>
     </div>
   );
 }
